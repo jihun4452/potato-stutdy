2fa365380496f9653bdeb9718a1a4d4968bd4184
박지훈 <jihun6548@naver.com>
2025-03-12 16:33:57 +0900

    fix: TestAdd

--- src/test/java/com/example/gamja/service/UserServiceTest.java
+++ src/test/java/com/example/gamja/service/UserServiceTest.java
@@ -1,14 +1,23 @@
 package com.example.gamja.service;
 
+import com.example.gamja.dto.UserCreateDto;
+import com.example.gamja.dto.UserStatus;
+import com.example.gamja.dto.UserUpdateDto;
 import com.example.gamja.exception.ResourceNotFoundException;
 import com.example.gamja.repository.UserEntity;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
@@ -20,6 +29,9 @@ public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    @MockitoBean
+    private JavaMailSender mailSender;
+
 
     @Test
     void getByEmail_ACTIVE_상태만_가져온다(){
@@ -38,17 +50,12 @@ public class UserServiceTest {
         //given
         String email = "test4@gmail.com";
 
-        //when
-
-        //then
         assertThatThrownBy(()->{
             UserEntity user=userService.getByEmail(email);
         }).isInstanceOf(ResourceNotFoundException.class);
     }
     @Test
     void getById_ACTIVE_상태만_가져온다(){
-        //given
-
         //when
         UserEntity user=userService.getById(85);
 
@@ -63,5 +70,39 @@ public class UserServiceTest {
             UserEntity user=userService.getById(2);
         }).isInstanceOf(ResourceNotFoundException.class);
     }
+
+    @Test
+    void Create_유저_생성(){
+
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("test10@gmail.com")
+                .address("seoul")
+                .nickname("test")
+                .build();
+
+        BDDMockito.doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        UserEntity user=userService.create(userCreateDto);
+
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getStatus()).isEqualTo(UserStatus.PENDING);
+    }
+
+    @Test
+    void Create_유저_정보_수정(){
+
+        UserUpdateDto userUpdateDtoe = UserUpdateDto.builder()
+                .address("mont")
+                .nickname("kkk")
+                .build();
+
+        userService.update(85, userUpdateDtoe);
+
+        UserEntity user=userService.getById(85);
+        assertThat(user.getId()).isNotNull();
+        assertThat(user.getAddress()).isEqualTo("mont");
+        assertThat(user.getNickname()).isEqualTo("kkk");
+    }
+
 
 }
